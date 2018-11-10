b0eb9dd1148e60a05e65134ea7885673463e03ff
N0Rbi <ksznorbi@gmail.com>
2018-11-10 13:48:57 +0100

    first working version

--- main1.cpp
+++ main1.cpp
@@ -1,42 +1,60 @@
-#include <iostream>
-
 using namespace std;
 
+/**
+A simple container tartalmazza a memoriaba lementett matrix ertekeket.
+*/
 template<class T, size_t N, size_t M>
 struct simple_container {
     T items[N][M];
 
-    T operator()(const std::size_t i, size_t j) const{
+    /**
+    visszaadunk egy konstans referenciat a tarolo (i,j). elemere
+    */
+    T operator()(const size_t i,  const size_t j) const{
       return items[i][j];
     }
 
-    T& operator()(const std::size_t i, size_t j){
+    /**
+    visszaadunk egy referenciat a tarolo (i,j). elemere, ekkor valtoztathato
+    az erteke
+    */
+    T& operator()(const size_t i, const size_t j){
       return items[i][j];
     }
 };
 
+/**
+const_mult_container letrehoz egy virtualis diagonalis matrixot,
+hogy a konstanst matrixszorzas segitsegevel szorozhassuk.
+*/
 template<class T>
 struct const_mult_container {
     T value;
 
     const_mult_container(T val) : value(val){};
 
-    T operator()(const std::size_t i, size_t j) const{
+    /**
+    csak akkor allitjuk az erteket 'value'-re, ha foatlobeli elemrol van szo
+    */
+    T operator()(const size_t i, size_t j) const{
       return (i == j) ? value : 0;
     }
 };
 
-template<class T>
-struct const_sum_container {
-    T value;
+// template<class T>
+// struct const_sum_container {
+//     T value;
+//
+//     const_sum_container(T val) : value(val){};
+//
+//     T operator()(const size_t i, size_t j) const{
+//       return value;
+//     }
+// };
 
-    const_sum_container(T val) : value(val){};
-
-    T operator()(const std::size_t i, size_t j) const{
-      return value;
-    }
-};
-
+/**
+sum_container ket azonos metertu matrixot var, virtualisan osszeadva oket.
+*/
 template<class T, class EXPR1, class EXPR2>
 struct sum_container {
   EXPR1 expr1;
@@ -44,11 +62,17 @@ struct sum_container {
 
   sum_container(const EXPR1& a, const EXPR2& b): expr1(a), expr2(b){}
 
-  T operator()(const std::size_t i, size_t j) const{
+  /**
+  a ket parameterul kapott matrix (i,j). elem osszeget adja vissza
+  */
+  T operator()(const size_t i, const size_t j) const{
     return expr1(i,j) + expr2(i,j);
   }
 };
 
+/**
+neg_container hasonló a sum_container-hez, csak kivonassal
+*/
 template<class T, class EXPR1, class EXPR2>
 struct neg_container {
   EXPR1 expr1;
@@ -56,46 +80,77 @@ struct neg_container {
 
   neg_container(const EXPR1& a, const EXPR2& b): expr1(a), expr2(b){}
 
-  T operator()(const std::size_t i, size_t j) const{
+  /**
+  az (i,j) parameterre visszaadja a ket parameterul kapott matrix
+  (i,j). elemeinek a kulonbseget
+  */
+  T operator()(const size_t i, const size_t j) const{
     return expr1(i,j) - expr2(i,j);
   }
 };
 
-// M is the matching index of the matrices
+/**
+mul_container egy N*M es M*K matrixok virtualis osszeszorzasara alkalmas
+*/
 template<class T, class EXPR1, class EXPR2, int M>
 struct mul_container {
   EXPR1 expr1;
   EXPR2 expr2;
 
   mul_container(const EXPR1& a, const EXPR2& b): expr1(a), expr2(b){ }
-
-  T operator()(const std::size_t i, size_t k) const{
+  /**
+  rekurziv fuggveny matrix szorzasra, az (i,k). elem megadja a definicio
+  szerinti erteket a ket matrix szorzatanak
+  a rekurzio alapja a ket matrix "kapcsolodasi dimenzioja"
+  */
+  T operator()(const size_t i, const size_t k) const{
     return (expr1(i,M-1) * expr2(M-1,k)) + (mul_container<T, EXPR1, EXPR2, M-1>(expr1, expr2))(i,k);
   }
 };
 
+/**
+a mul_container specializacioja, ha a "kapcsolodo dimenzio" 0 akkor az (i, k).
+elem sem lehet kulonbozo 0-nal.
+*/
 template<class T,  class EXPR1, class EXPR2>
 struct mul_container <T, EXPR1, EXPR2, 0> {
 
   mul_container(const EXPR1& a, const EXPR2& b){}
 
-  T operator()(const std::size_t i, size_t k) const{
+  /**
+  leszallasi feltetel matrix szorzashoz
+  */
+  T operator()(const size_t i, const size_t k) const{
     return 0;
   }
 };
 
+/**
+my_matrix az alap matrix osztaly, mely a fent talalhato segely osztalyokat
+hasznalja fel kontenerkent
+*/
 template<class T, size_t N, size_t M, class Cont= simple_container<T, N, M> >
-class my_matrix{
+class my_matrix {
   Cont cont;
 
 public:
+  /**
+  alapertelemezett konstruktor
+  */
   my_matrix() {}
+  /**
+  konstruktor virtualizalt elemekhez
+  */
   my_matrix(const Cont& other) : cont(other){}
 
-  // assignment operator for MyVector of different type
+  /**
+  ertekadas operator a simple_container-ekkel elvegzi a virtualis szamitasokat,
+  egeszen az adott "other" objektum kontenereig, majd beallirja ertekul az
+  aktualis objektumnak
+  */
   template<class T2, class Cont2>
   my_matrix& operator=(const my_matrix<T2, N, M, Cont2>& other){
-    for (std::size_t i = 0; i < N; ++i)
+    for (size_t i = 0; i < N; ++i)
       for (size_t j = 0; j < M; j++)
       {
         cont(i,j) = other(i,j);
@@ -103,134 +158,100 @@ public:
     return *this;
   }
 
-  // index operators
-  T operator()(const std::size_t i, size_t j) const{
+  /**
+  (i,j). elem ertek szerinti lekerese
+  */
+  T operator()(const size_t i, const size_t j) const{
     return cont(i,j);
   }
 
-  T& operator()(const std::size_t i, size_t j) {
+  /**
+  (i,j). elem referencia szerinti lekerese
+  */
+  T& operator()(const size_t i, const size_t j) {
     return cont(i,j);
   }
 
-  // returns the underlying data
+  /**
+  visszaadja a kontener konstans referenciajat
+  */
   const Cont& data() const{
     return cont;
   }
 
+  /**
+  visszaadja a kontener referenciajat
+  */
   Cont& data(){
     return cont;
   }
 
+  /**
+  virtualis osszeadas matrixok kozott
+  */
   template<class Other_cont>
   my_matrix<T, N, M, sum_container<T, Cont, Other_cont> > operator+(const my_matrix<T, N, M, Other_cont>& other){
     return my_matrix<T, N, M, sum_container<T, Cont, Other_cont> > (sum_container<T, Cont, Other_cont>(this->data(), other.data()));
   }
 
+  /**
+  virtualis osszeadas matrixok kozott (konstans)
+  */
   template<class Other_cont>
   my_matrix<T, N, M, sum_container<T, Cont, Other_cont> > operator+(const my_matrix<T, N, M, Other_cont>& other) const{
     return my_matrix<T, N, M, sum_container<T, Cont, Other_cont> > (sum_container<T, Cont, Other_cont>(this->data(), other.data()));
   }
 
-  my_matrix<T, N, M, sum_container<T, Cont, const_sum_container<T> > > operator+(const T& other) {
-    return this->operator+(my_matrix<T, N, M, const_sum_container<T> >(const_sum_container<T>(other)));
-  }
-
-  my_matrix<T, N, M, sum_container<T, Cont, const_sum_container<T> > > operator+(const T& other) const {
-    return this->operator+(my_matrix<T, N, M, const_sum_container<T> >(const_sum_container<T>(other)));
-  }
-
-  friend my_matrix<T, N, M, sum_container<T, Cont, const_sum_container<T> > > operator+(const T c, const my_matrix<T, N, M, Cont>& mtx) {
-    return mtx.operator+(c);
-  }
-
+  /**
+  virtualis kivonas matrixok kozott
+  */
   template<class Other_cont>
   my_matrix<T, N, M, neg_container<T, Cont, Other_cont> > operator-(const my_matrix<T, N, M, Other_cont>& other){
     return my_matrix<T, N, M, neg_container<T, Cont, Other_cont> > (neg_container<T, Cont, Other_cont>(this->data(), other.data()));
   }
 
+  /**
+  virtualis kivonas matrixok kozott (konstans)
+  */
   template<class Other_cont>
   my_matrix<T, N, M, neg_container<T, Cont, Other_cont> > operator-(const my_matrix<T, N, M, Other_cont>& other) const {
     return my_matrix<T, N, M, neg_container<T, Cont, Other_cont> > (neg_container<T, Cont, Other_cont>(this->data(), other.data()));
   }
 
-  my_matrix<T, N, M, neg_container<T, Cont, const_sum_container<T> > > operator-(const T other){
-    return this->operator-(my_matrix<T, N, M, const_sum_container<T> >(const_sum_container<T>(other)));
-  }
-
+  /**
+  virtualis szorzas matrixok kozott
+  */
   template<size_t K, class Other_cont>
   my_matrix<T, N, K, mul_container<T, Cont, Other_cont, M> > operator*(const my_matrix<T, M, K, Other_cont>& other){
     return my_matrix<T, N, K, mul_container<T, Cont, Other_cont, M> > (mul_container<T, Cont, Other_cont, M>(this->data(), other.data()));
   }
 
+  /**
+  virtualis szorzas matrixok kozott (konstans)
+  */
+  template<size_t K, class Other_cont>
+  my_matrix<T, N, K, mul_container<T, Cont, Other_cont, M> > operator*(const my_matrix<T, M, K, Other_cont>& other) const {
+    return my_matrix<T, N, K, mul_container<T, Cont, Other_cont, M> > (mul_container<T, Cont, Other_cont, M>(this->data(), other.data()));
+  }
+
+  /**
+  virtualis szorzas matrix es konstans kozott
+  */
   my_matrix<T, N, M, mul_container<T, Cont, const_mult_container<T>, M> > operator*(const T other){
     return this->operator*(my_matrix<T, N, M, const_mult_container<T> >(const_mult_container<T>(other)));
   }
+
+  /**
+  virtualis szorzas matrix es konstans kozott (konstans)
+  */
+  my_matrix<T, N, M, mul_container<T, Cont, const_mult_container<T>, M> > operator*(const T other) const {
+    return this->operator*(my_matrix<T, N, M, const_mult_container<T> >(const_mult_container<T>(other)));
+  }
+
+  /**
+  virtualis szorzas konstans es matrix kozott
+  */
+  friend my_matrix<T, N, M, mul_container<T, Cont, const_mult_container<T>, M> > operator*(const T c, const my_matrix<T, N, M, Cont>& mtx) {
+    return mtx.operator*(c);
+  }
 };
-
-int main() {
-    my_matrix<int,2,2> mtx1;
-    my_matrix<int,2,2> mtx2;
-    my_matrix<int,2,2> mtx3;
-    my_matrix<int,2,2> mtx4;
-    my_matrix<int,1,1> mtx5;
-
-    for (size_t i=0; i<2; i++) {
-        for (size_t j=0; j<2; j++) {
-            mtx1(i,j) = 1;
-            mtx2(i,j) = 2;
-            mtx3(i,j) = 3;
-        }
-    }
-
-
-    for (size_t i=0; i<2; i++) {
-        for (size_t j=0; j<2; j++) {
-            cout << mtx1(i,j) << "\t";
-        }
-        cout << endl;
-      }
-      cout << endl;
-
-      for (size_t i=0; i<2; i++) {
-          for (size_t j=0; j<2; j++) {
-              cout << mtx2(i,j) << "\t";
-          }
-          cout << endl;
-        }
-        cout << endl;
-
-        mtx1 = 10 + mtx1;
-
-        for (size_t i=0; i<2; i++) {
-            for (size_t j=0; j<2; j++) {
-                cout << mtx1(i,j) << "\t";
-            }
-            cout << endl;
-          }
-          cout << endl;
-
-          for (size_t i=0; i<2; i++) {
-              for (size_t j=0; j<2; j++) {
-                  cout << mtx2(i,j) << "\t";
-              }
-              cout << endl;
-            }
-            cout << endl;
-    /*mtx1 = mtx1*3;
-
-    for (size_t i=0; i<2; i++) {
-        for (size_t j=0; j<3; j++) {
-            mtx5(i,j) = 1;
-        }
-    }
-
-    mtx4 = mtx1 + mtx2;
-
-    for (size_t i=0; i<2; i++) {
-        for (size_t j=0; j<3; j++) {
-            cout << mtx4(i,j) << endl;
-        }
-    }*/
-
-    return 0;
-}
